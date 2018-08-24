83c8951309c33838aa527a5ff581618bb1e8c560
csuprolifics <41317732+csuprolifics@users.noreply.github.com>
2018-08-25 00:08:14 +1000

    author and reviewer details added
    author and reviewer details added

--- src/member.java
+++ src/member.java
@@ -5,24 +5,24 @@ import java.util.List;
 import java.util.Map;
 
 @SuppressWarnings("serial")
-public class Member implements Serializable {					//class name changed member to Member
+public class Member implements Serializable {					//class name changed member to Member author @anjli reviewer @suresh
 
-	private String lname;							//Variable name changed LN to lname
-	private String fname;							//Variable name changed FN to fname
-	private String emailid;							//Variable name changed EM to emailid
-	private int pno;							//Variable name changed PN to pno
-	private int iD;								//variable name changed ID to iD
-	private double fines;							//Variable name Changed FINES to fines
+	private String lname;							//Variable name changed LN to lname author @anjli reviewer @suresh
+	private String fname;							//Variable name changed FN to fname author @anjli reviewer @suresh
+	private String emailid;							//Variable name changed EM to emailid author @anjli reviewer @suresh
+	private int pno;							//Variable name changed PN to pno author @anjli reviewer @suresh
+	private int iD;								//variable name changed ID to iD author @anjli reviewer @suresh
+	private double fines;							//Variable name Changed FINES to fines author @anjli reviewer @suresh
 	
 	private Map<Integer, loan> LNS;
 
 	
 	public member(String lastName, String firstName, String email, int phoneNo, int id) {
-		this.lname = lastName;						//Changed this.LN to this.lname
-		this.fname = firstName;						//changed this.FN to this.fname
-		this.emailid = email;						//Changed this.EM to this.emailid
-		this.pno = phoneNo;						//Changed this.PN to this.pno 
-		this.iD = id;							//Changed this.ID to this.iD
+		this.lname = lastName;						//Changed this.LN to this.lname author @anjli reviewer @suresh
+		this.fname = firstName;						//changed this.FN to this.fname author @anjli reviewer @suresh
+		this.emailid = email;						//Changed this.EM to this.emailid author @anjli reviewer @suresh
+		this.pno = phoneNo;						//Changed this.PN to this.pno author @anjli reviewer @suresh
+		this.iD = id;							//Changed this.ID to this.iD author @anjli reviewer @suresh
 		
 		this.LNS = new HashMap<>();
 	}
@@ -30,13 +30,13 @@ public class Member implements Serializable {					//class name changed member to
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Member:  ").append(iD).append("\n");				//; included and changed ID to iD
-		sb.append("  Name:  ").append(lname).append(", ").append(FN).append("\n");  //object and ; included and changed LN to lname
-		sb.append("  Email: ").append(emailid).append("\n");				//object and ; included and changed EM to emailid
-		sb.append("  Phone: ").append(pno);					//object and ; included and changed PN to pno
-		sb.append("\n");							//object and ; included
-		sb.append(String.format("  Fines Owed :  $%.2f", fines));		//object and ; included and changed FINES to fines
-		sb.append("\n");							//object and ; included
+		sb.append("Member:  ").append(iD).append("\n");				//; included and changed ID to iD author @anjli reviewer @suresh
+		sb.append("  Name:  ").append(lname).append(", ").append(FN).append("\n");  //object and ; included and changed LN to lname author @anjli reviewer @suresh
+		sb.append("  Email: ").append(emailid).append("\n");				//object and ; included and changed EM to emailid author @anjli reviewer @suresh
+		sb.append("  Phone: ").append(pno);					//object and ; included and changed PN to pno author @anjli reviewer @suresh
+		sb.append("\n");							//object and ; included author @anjli reviewer @suresh
+		sb.append(String.format("  Fines Owed :  $%.2f", fines));		//object and ; included and changed FINES to fines author @anjli reviewer @suresh
+		sb.append("\n");							//object and ; included author @anjli reviewer @suresh
 		
 		for (loan loan : LNS.values()) {
 			sb.append(loan).append("\n");
@@ -46,7 +46,7 @@ public class Member implements Serializable {					//class name changed member to
 
 	
 	public int getId() {
-		return iD;						//changed ID to iD
+		return iD;						//changed ID to iD author @anjli reviewer @suresh
 	}
 
 	
@@ -61,7 +61,7 @@ public class Member implements Serializable {					//class name changed member to
 
 	
 	public double getFinesOwed() {
-		return fines;						// Changed FINES to fines
+		return fines;						// Changed FINES to fines author @anjli reviewer @suresh
 	}
 
 	
@@ -76,12 +76,12 @@ public class Member implements Serializable {					//class name changed member to
 
 	
 	public String getLastName() {
-		return lname;						//Changed LN to lname
+		return lname;						//Changed LN to lname author @anjli reviewer @suresh
 	}
 
 	
 	public String getFirstName() {
-		return fname;    					//Changed FN to fname
+		return fname;    					//Changed FN to fname author @anjli reviewer @suresh
 	}
 
 
